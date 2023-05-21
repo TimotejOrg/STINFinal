cf19fe3a0fb928c5fc622c35f8cc926567d90313
TimotejFasiang <60715817+TimotejFasiang@users.noreply.github.com>
2023-05-21 14:37:50 +0200

    Connected website forms to flask and prepared form validity checks

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from formChecks import *
 
 app = Flask(__name__)
 
@@ -8,24 +9,72 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/register/')
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        registration_info = [request.form['firstName'], request.form['lastName'], request.form['email'],
+                             request.form['password'], request.form['currency'], request.form['securityAnswer']]
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        currency = request.form['currency']
+        security_answer = request.form['securityAnswer']
+        print(registration_info)
+        if checkRegistration(registration_info):
+            return redirect('/login/')
+        else:
+            return "Registration error"
+    else:
+        return render_template('register.html')
 
 
-@app.route('/merchant-payment/')
+@app.route('/merchant-payment/', methods=['GET', 'POST'])
 def merchant_payment():
-    return render_template('merchant-payment.html')
+    if request.method == 'POST':
+        merchant_info = [request.form['amount'], request.form['currency'], request.form['merchantAccount'],
+                         request.form['email'], request.form['password']]
+        amount = request.form['amount']
+        currency = request.form['currency']
+        merchant_account = request.form['merchantAccount']
+        email = request.form['email']
+        password = request.form['password']
+        print(merchant_info)
+        if checkMerchantInfo(merchant_info):
+            return redirect('/')
+        else:
+            return "Merchant info error"
+    else:
+        return render_template('merchant-payment.html')
 
 
-@app.route('/login/')
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        login_info = [request.form['email'], request.form['password']]
+        email = request.form['email']
+        password = request.form['password']
+        print(login_info)
+        if checkLogin(login_info):
+            return redirect('/login/')
+        else:
+            return "Login error"
+    else:
+        return render_template('login.html')
 
 
-@app.route('/security-question/')
+@app.route('/security-question/', methods=['GET', 'POST'])
 def security_question():
-    return render_template('security-question.html')
+    if request.method == 'POST':
+        security_answer_info = [request.form['securityAnswer']]
+        security_answer = request.form['securityAnswer']
+        print(security_answer_info)
+        if checkSecurityAnswer(security_answer_info):
+            return redirect('/login/')
+        else:
+            return "Security answer error"
+    else:
+        return render_template('security-question.html')
 
 
 @app.route('/account/')
@@ -33,9 +82,19 @@ def account():
     return render_template('account.html')
 
 
-@app.route('/account/deposit/')
+@app.route('/account/deposit/', methods=['GET', 'POST'])
 def deposit():
-    return render_template('deposit.html')
+    if request.method == 'POST':
+        deposit_info = [request.form['amount'], request.form['currency']]
+        amount = request.form['amount']
+        currency = request.form['currency']
+        print(deposit_info)
+        if checkDeposit(deposit_info):
+            return redirect('/account/')
+        else:
+            return "Deposit error"
+    else:
+        return render_template('deposit.html')
 
 
 @app.route('/account/bank-statement/')
---
+++ formChecks.py
@@ -0,0 +1,18 @@
+def checkRegistration(registration_info):
+    pass
+
+
+def checkMerchantInfo(merchant_info):
+    pass
+
+
+def checkLogin(login_info):
+    pass
+
+
+def checkSecurityAnswer(security_answer_info):
+    pass
+
+
+def checkDeposit(deposit_info):
+    pass
